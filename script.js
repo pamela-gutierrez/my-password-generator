ff87ab7d1158d94cc28ef6748267c2ad10844d1d
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2020-12-19 17:17:08 -0800

    added comments

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ passwordValue = numberValue
 
 function writePassword() { 
  var characterCount = prompt("How many characters would you like your password to contain?")
-
+// characterCount variable will determine the lenght of the password based on what the user enters.
 if (characterCount <= 7){
     alert ("Password must be at least 8 characters.")
   } else if (characterCount >= 129){
@@ -46,99 +46,40 @@ if (characterCount <= 7){
   var specialCharacterQuestion = window.confirm("Do you want to include special characters")    
   var numberQuestion = window.confirm("Do you want numbers?")
   }
+
+  // If statements  that will determine what character types returned in the password
   password = ""
   if (upperCaseQuestion === true){
-    for (i = 0; i <= characterCount; i++)
-    password = ""
+    for (i = 0; i <= characterCount; i++){
+    // password = ""
     password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(upperCaseValue.length - 1 )))
+    }
   }
   if (lowerCaseQuestion === true){
-    for (i = 0; i <= characterCount; i++)
-    password = ""
+    for (i = 0; i <= characterCount; i++){
+    // password = ""
     password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(lowerCaseValue.length - 1 )))
+    }
   }
   if (specialCharacterQuestion === true){
-  for (i = 0; i <= characterCount; i++)
-  password = ""
-  password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(specialCharacterValue.length - 1 )))
+    for (i = 0; i <= characterCount; i++){
+    // password = ""
+    password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(specialCharacterValue.length - 1 )))
+    }
   }
   if (numberQuestion === true){
-  for (i = 0; i <= characterCount; i++)
-  password = ""
-  password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(numberValue.length - 1 )))
+    for (i = 0; i <= characterCount; i++){
+    // password = ""
+    password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(numberValue.length - 1 )))
+    }
   }
-} 
-
+// } 
   // var password = generatePassword();
+  console.log(password)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password
-
-
+}
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// If no to all conditions alert "You must select at least one kind of character."
-
-// var upperCaseWanted = false;
-// var lowerCaseWanted = false;
-// var specialCharacterWanted = false;
-
-// if (upperCaseQuestion === true) {
-//   upperCaseWanted = true
-// }
-
-// if (lowerCaseQuestion === true) {
-//   lowerCaseWanted = true
-// }
-
-// if (specialCharacter === true) {
-//   specialCharacter = true
-// }
-
-// }
-
-// var upperCaseQuestion = window.confirm("Do you want to include uppercase letters?")
-// var lowerCaseQuestion = window.confirm("Do you want lowercase letters?")
-
-
-
-
-
-
-
-// Take whatever number of characters the user selects in the character count and generate a password of that length. 
-// Need to put together a password made of character choices- numbers, upper and lower case letters, symbols.
-// Need to create an alert if the user doesn't confirm at least one of the character types. 
-// Push or return password comprised on all different characteristics. 
-
-
-
-
-//  I need the window to prompt a questions. 
-// "How many characters would you like your password to contain?"
-// Any number less than 8 should cause an alert that says "Password length must be at least 8 characters?"
-// Any number less more than 128 should cause an alert that says "Password length should be less than 129 characters."
-
-
-// Then I need to write an if statement and establish the different possible outcomes.
-// Then I need to set the parameters for those answers. In this case it'll be at least 8 and no more than 128 characters. 
-// var upperCaseWanted = false;
-// var numbersWanted = false;
-
-// var upperCaseQuestion = (window.confirm "Do you want to include uppercase?")
-// var numbersQuestion =
-// if (upperCaseQuestion = window.confirm("Do you want to include uppercase characters?")
-// if (upperCaseLetter.)
-
-
-// // returns a random number from 8-128
-// Math.floor(Math.random() * 128) + 8;
-// console.log()
-// var generatePassword = [];
-// var upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I" ,"J" , "K" , "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t", "u", "v", "w", "x", "y", "z"];
-// var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")" ];
